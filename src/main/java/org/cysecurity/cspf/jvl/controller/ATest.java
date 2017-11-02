4f75767d486fac945b154683889c528363aba7fa
sharath1817 <dasarisharath1817@gmail.com>
2017-11-02 11:04:41 +0530

    sss

--- src/main/java/org/cysecurity/cspf/jvl/controller/ATest.java
+++ src/main/java/org/cysecurity/cspf/jvl/controller/ATest.java
@@ -1,3 +1,41 @@
+package org.cysecurity.cspf.jvl.model;
+
+ 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+
+
+public class DBConnect {
+    public Connection connect(String path) throws IOException,ClassNotFoundException,SQLException
+    {
+        Properties properties=new Properties();
+        properties.load(new FileInputStream(path));
+        String dbuser=properties.getProperty("dbuser");
+         String dbpass = properties.getProperty("dbpass");
+       String dbfullurl = properties.getProperty("dburl")+properties.getProperty("dbname");
+       String jdbcdriver = properties.getProperty("jdbcdriver");
+            Connection con=null;
+          try
+                   {
+                    Class.forName(jdbcdriver);
+                    con= DriverManager.getConnection(dbfullurl,dbuser,dbpass);
+                    return con;
+                   }
+                   finally
+                    {
+                                 
+                    }      
+    }
+}
+
+
+
 public User getUser(Connection con, String user) throws SQLException {
 
   Statement stmt1 = null;
