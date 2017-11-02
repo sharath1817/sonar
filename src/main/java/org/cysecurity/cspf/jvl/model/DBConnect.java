aa44a090b1a4e7c96f8c0c59f8be2b7663226f40
sharath1817 <dasarisharath1817@gmail.com>
2017-11-02 11:37:14 +0530

    ssd

--- src/main/java/org/cysecurity/cspf/jvl/model/DBConnect.java
+++ src/main/java/org/cysecurity/cspf/jvl/model/DBConnect.java
@@ -10,9 +10,6 @@ package org.cysecurity.cspf.jvl.model;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.sql.*;
-import java.util.*;
-import java.io.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -25,26 +22,6 @@ import java.util.Properties;
 public class DBConnect {
     public Connection connect(String path) throws IOException,ClassNotFoundException,SQLException
     {
-		String query=null;
-		String userid = request.getParameter("userid");
-	String password = request.getParameter("password");
-	Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
-	connection = DriverManager.getConnection("jdbc:odbc:projectDB");
-
-	query = "SELECT * FROM Users WHERE user_id ='" + userid + "' AND password ='" + password +"'";
-
-	PreparedStatement ps = connection.prepareStatement(query);
-	ResultSet users = ps.executeQuery();
-
-	if(users.next()){
-
-	//some thing here
-	}
-	else{
-
-		}
-		 String dbpass=null;
-		 String con=null;
         Properties properties=new Properties();
         properties.load(new FileInputStream(path));
         String dbuser=properties.getProperty("dbuser");
@@ -52,8 +29,6 @@ public class DBConnect {
        String dbfullurl = properties.getProperty("dburl")+properties.getProperty("dbname");
        String jdbcdriver = properties.getProperty("jdbcdriver");
            
-			
-		 
           try
                    {
                     Class.forName(jdbcdriver);
